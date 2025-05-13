4b548993a69a06622bc8233582991ae9d8e51a6c
mohammadne <mohammadne.dev@gmail.com>
2025-05-13 10:09:38 +0330

    chore: add base of http handlers

--- internal/api/http/handlers/poll.go
+++ internal/api/http/handlers/poll.go
@@ -15,15 +15,61 @@ func NewPoll(r fiber.Router, logger *zap.Logger) {
 	}
 
 	g := r.Group("poll")
-	g.Post("/", handler.todo)
+	g.Post("/", handler.createPoll)
+	g.Get("/", handler.retrieveFeed)
+	g.Get("/:id/vote", handler.vote)
+	g.Get("/:id/skip", handler.skip)
+	g.Get("/:id/stats", handler.statistics)
 }
 
 type poll struct {
 	logger *zap.Logger
 }
 
-func (s *poll) todo(c fiber.Ctx) error {
+func (s *poll) createPoll(c fiber.Ctx) error {
 	response := &models.Response{}
+
+	return response.Write(c, http.StatusInternalServerError)
+}
+
+func (s *poll) retrieveFeed(c fiber.Ctx) error {
+	response := &models.Response{}
+
+	return response.Write(c, http.StatusInternalServerError)
+}
+
+func (s *poll) vote(c fiber.Ctx) error {
+	response := &models.Response{}
+
+	id := c.Params("id")
+	if len(id) == 0 {
+		s.logger.Error("poll id not given for vote")
+		return response.Write(c, fiber.StatusBadRequest)
+	}
+
+	return response.Write(c, http.StatusInternalServerError)
+}
+
+func (s *poll) skip(c fiber.Ctx) error {
+	response := &models.Response{}
+
+	id := c.Params("id")
+	if len(id) == 0 {
+		s.logger.Error("poll id not given for skip")
+		return response.Write(c, fiber.StatusBadRequest)
+	}
+
+	return response.Write(c, http.StatusInternalServerError)
+}
+
+func (s *poll) statistics(c fiber.Ctx) error {
+	response := &models.Response{}
+
+	id := c.Params("id")
+	if len(id) == 0 {
+		s.logger.Error("poll id not given for statistics")
+		return response.Write(c, fiber.StatusBadRequest)
+	}
 
 	return response.Write(c, http.StatusInternalServerError)
 }
